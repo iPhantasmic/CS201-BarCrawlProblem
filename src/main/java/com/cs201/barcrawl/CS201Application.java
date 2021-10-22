ef161d465cbfaa8d10d22ac1c3648b1b9aadef6a
Justin <justin.100600@gmail.com>
2021-10-23 01:58:16 +0800

    Update .gitignore

---
+++ src/main/java/com/cs201/barcrawl/CS201Application.java
@@ -0,0 +1,13 @@
+package com.cs201.barcrawl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CS201Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CS201Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/cs201/barcrawl/CS201ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs201.barcrawl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CS201ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
