836a6cd9165321e3387082a868132a532edfb56b
Gary Peabody <aikarvin99@gmail.com>
2021-10-25 16:09:31 +0800

    Added google maps connection to find distances between businesses
    Signed-off-by: Gary Peabody <aikarvin99@gmail.com>

--- src/main/java/com/cs201/barcrawl/controller/BusinessController.java
+++ src/main/java/com/cs201/barcrawl/controller/BusinessController.java
@@ -1,12 +1,18 @@
 package com.cs201.barcrawl.controller;
 
+import com.cs201.barcrawl.models.Business;
 import com.cs201.barcrawl.service.BusinessService;
+import com.cs201.barcrawl.util.GoogleMapsUtil;
+import com.squareup.okhttp.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
@@ -15,6 +21,9 @@ public class BusinessController {
 
     @Autowired
     BusinessService businessService;
+
+    @Autowired
+    GoogleMapsUtil googleMapsUtil;
 
     @GetMapping(value = "/test")
     public @ResponseBody ResponseEntity<?> TestApi() {
@@ -26,5 +35,14 @@ public class BusinessController {
     public @ResponseBody ResponseEntity<?> loadData() {
         boolean result = businessService.loadData();
         return ResponseEntity.ok(result);
+    }
+
+    @GetMapping(value = "/gmapsTest")
+    public Response distanceMatrix(){
+        Business origin = businessService.getBusiness(1);
+        List<Business> destinations = new ArrayList<>();
+        destinations.add(businessService.getBusiness(2));
+        destinations.add(businessService.getBusiness(3));
+        return googleMapsUtil.distanceMatrix(origin, destinations);
     }
 }
