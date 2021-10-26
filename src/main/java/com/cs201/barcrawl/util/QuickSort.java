1406e6243c848d4a749f4638d351c3d8dae3b86d
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-26 15:51:05 +0800

    Add quicksort logic
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/com/cs201/barcrawl/util/QuickSort.java
+++ src/main/java/com/cs201/barcrawl/util/QuickSort.java
@@ -1,5 +1,17 @@
 package com.cs201.barcrawl.util;
 
 public class QuickSort<T extends Comparable<? super T>> {
-    
+    void quicksort(T[] array, int startIndex, int endIndex)
+    {
+        // verify that the start and end index have not overlapped
+        if (startIndex < endIndex)
+        {
+            // calculate the pivotIndex
+            int pivotIndex = partition(array, startIndex, endIndex);
+            // sort the left sub-array
+            quicksort(array, startIndex, pivotIndex);
+            // sort the right sub-array
+            quicksort(array, pivotIndex + 1, endIndex);
+        }
+    }
 }
