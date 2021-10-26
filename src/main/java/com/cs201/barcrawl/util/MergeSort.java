54cb528554650b05c6fd9de109fd5dcb9a2f2ca9
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-26 15:44:12 +0800

    Add mergeSort function
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/com/cs201/barcrawl/util/MergeSort.java
+++ src/main/java/com/cs201/barcrawl/util/MergeSort.java
@@ -1,4 +1,24 @@
 package com.cs201.barcrawl.util;
 
-public class MergeSort {
+// Implementing merge sort for a generic type
+public class MergeSort<T extends Comparable<? super T>> {
+
+    // mergeSort takes in an array of object of type T and the start and the end
+    void mergeSort(T[] array, int start, int end) {
+        if (start < end)
+        {
+            // find the middle index
+            int middle = (start + end) / 2;
+
+            // sort first half
+            mergeSort(array, start, middle);
+            // sort second half
+            mergeSort(array, middle + 1, end);
+
+            // merge the sorted halves
+            merge(array, start, middle, end);
+        }
+    }
+
+    
 }
