69fccd23bc8309a0fae9d4d221e6ed3c43bc78d0
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-26 15:46:13 +0800

    Add merging logic
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/com/cs201/barcrawl/util/MergeSort.java
+++ src/main/java/com/cs201/barcrawl/util/MergeSort.java
@@ -32,5 +32,33 @@ public class MergeSort<T extends Comparable<? super T>> {
         // fill in right array
         for (int i = 0; i < rightArray.length; ++i)
             rightArray[i] = array[middle + 1 + i];
+
+        // initial indexes of first and second subarrays
+        int leftIndex = 0, rightIndex = 0;
+
+        // the index we will start at when adding the subarrays back into the main array
+        int currentIndex = start;
+
+        // compare each index of the subarrays adding the lowest value to the currentIndex
+        while (leftIndex < leftArray.length && rightIndex < rightArray.length)
+        {
+            if (leftArray[leftIndex].compareTo(rightArray[rightIndex]) <= 0)
+            {
+                array[currentIndex] = leftArray[leftIndex];
+                leftIndex++;
+            }
+            else
+            {
+                array[currentIndex] = rightArray[rightIndex];
+                rightIndex++;
+            }
+            currentIndex++;
+        }
+
+        // copy remaining elements of leftArray[] if any
+        while (leftIndex < leftArray.length) array[currentIndex++] = leftArray[leftIndex++];
+
+        // copy remaining elements of rightArray[] if any
+        while (rightIndex < rightArray.length) array[currentIndex++] = rightArray[rightIndex++];
     }
 }
