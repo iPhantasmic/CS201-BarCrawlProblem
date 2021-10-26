34cb8a463c7f6df73b37296f8f6ea6ac0a6a41ed
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-26 15:51:28 +0800

    Add partition logic
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/com/cs201/barcrawl/util/QuickSort.java
+++ src/main/java/com/cs201/barcrawl/util/QuickSort.java
@@ -14,4 +14,30 @@ public class QuickSort<T extends Comparable<? super T>> {
             quicksort(array, pivotIndex + 1, endIndex);
         }
     }
+
+    private int partition(T[] array, int startIndex, int endIndex)
+    {
+        int pivotIndex = (startIndex + endIndex) / 2;
+        T pivotValue = array[pivotIndex];
+        startIndex--;
+        endIndex++;
+
+        while (true)
+        {
+            // start at the FIRST index of the sub-array and increment
+            // FORWARD until we find a value that is > pivotValue
+            do startIndex++; while (array[startIndex].compareTo(pivotValue) < 0) ;
+
+            // start at the LAST index of the sub-array and increment
+            // BACKWARD until we find a value that is < pivotValue
+            do endIndex--; while (array[endIndex].compareTo(pivotValue) > 0) ;
+
+            if (startIndex >= endIndex) return endIndex;
+
+            // swap values at the startIndex and endIndex
+            T temp = array[startIndex];
+            array[startIndex] = array[endIndex];
+            array[endIndex] = temp;
+        }
+    }
 }
