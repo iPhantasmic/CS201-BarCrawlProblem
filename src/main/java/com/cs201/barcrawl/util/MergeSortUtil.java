37ade810403ac1e01f58e8b634e02572bc1dfe42
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-31 17:18:22 +0800

    Update merge sort util name
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/com/cs201/barcrawl/util/MergeSortUtil.java
+++ src/main/java/com/cs201/barcrawl/util/MergeSortUtil.java
@@ -1,7 +1,10 @@
 package com.cs201.barcrawl.util;
 
+import org.springframework.stereotype.Component;
+
 // Implementing merge sort for a generic type
 // Taken from: https://big-o.io/examples/merge-sort/java-generic/
+@Component
 public class MergeSort<T extends Comparable<? super T>> {
 
     // mergeSort takes in an array of object of type T and the start and the end
