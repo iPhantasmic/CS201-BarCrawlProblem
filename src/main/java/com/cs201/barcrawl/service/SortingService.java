7260be63f7165aad2b82306c81b3d8e05f6097a3
Bing Yu <bingyu.yap.21@gmail.com>
2021-11-01 14:05:05 +0800

    service: SortingService: Add quickSort logic
    Signev-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/com/cs201/barcrawl/service/SortingService.java
+++ src/main/java/com/cs201/barcrawl/service/SortingService.java
@@ -2,6 +2,7 @@ package com.cs201.barcrawl.service;
 
 import com.cs201.barcrawl.models.Business;
 import com.cs201.barcrawl.util.MergeSortUtil;
+import com.cs201.barcrawl.util.QuickSortUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,9 @@ public class SortingService {
     @Autowired
     private MergeSortUtil mergeSortUtil;
 
+    @Autowired
+    private QuickSortUtil quickSortUtil;
+
     public Business[] wrap(List<Business> businesses) {
         return businesses.toArray(Business[]::new);
     }
@@ -21,6 +25,14 @@ public class SortingService {
         Business[] businessArray = wrap(businesses);
 
         mergeSortUtil.mergeSort(businessArray, 0, businessArray.length - 1);
+
+        return businessArray;
+    }
+
+    public Business[] quickSort(List<Business> businesses) {
+        Business[] businessArray = wrap(businesses);
+
+        quickSortUtil.quicksort(businessArray, 0, businessArray.length - 1);
 
         return businessArray;
     }
