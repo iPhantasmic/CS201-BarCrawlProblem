c649394979eb37a5b5f5c558b28cc5902427cc07
Bing Yu <bingyu.yap.21@gmail.com>
2021-11-01 14:50:00 +0800

    models: SortedDTO: Add getter and setters
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/com/cs201/barcrawl/models/SortedDTO.java
+++ src/main/java/com/cs201/barcrawl/models/SortedDTO.java
@@ -4,4 +4,20 @@ public class SortedDTO {
     private int time;
 
     private Business[] destinations;
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public Business[] getDestinations() {
+        return destinations;
+    }
+
+    public void setDestinations(Business[] destinations) {
+        this.destinations = destinations;
+    }
 }
